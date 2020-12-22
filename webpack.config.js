35b1724855197aa73a781c46240ae613059da1aa
mAnic <arctic.manic@gmail.com>
2020-12-22 18:49:12 +0300

    module1-task3

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CircularDependencyPlugin = require('circular-dependency-plugin')
 
 module.exports = {
   entry: './src/main.js',
@@ -10,5 +11,14 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     watchContentBase: true,
-  }
+  },
+  plugins: [
+    new CircularDependencyPlugin({
+      exclude: /a\.js|node_modules/,
+      include: /dir/,
+      failOnError: true,
+      allowAsyncCycles: false,
+      cwd: process.cwd(),
+    })
+  ]
 };
