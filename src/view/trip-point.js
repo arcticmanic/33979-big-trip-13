ca9bccf60822d4cc32847ae4f8dc407484450164
mAnic <arctic.manic@gmail.com>
2020-12-27 22:32:37 +0300

    module2-task1

--- src/view/trip-point.js
+++ src/view/trip-point.js
@@ -1,35 +1,68 @@
-const createTripPointTemplate = () => {
+import dayjs from "dayjs";
+import {toFormattedString} from "../utils.js";
+
+const MINUTES_PER_HOUR = 60;
+const MINUTES_PER_DAY = 1440;
+
+const getEventDuration = (startTime, endTime) => {
+  const durationInMinutes = endTime.diff(startTime, `minute`);
+  if (durationInMinutes < MINUTES_PER_HOUR) {
+    return `${toFormattedString(durationInMinutes, 2)}M`;
+  } else if (durationInMinutes < MINUTES_PER_DAY) {
+    const hours = Math.floor(durationInMinutes / MINUTES_PER_HOUR);
+    const minutes = durationInMinutes % MINUTES_PER_HOUR;
+    return `${toFormattedString(hours, 2)}H ${toFormattedString(minutes, 2)}M`;
+  } else {
+    const days = Math.floor(durationInMinutes / MINUTES_PER_DAY);
+    const hours = Math.floor((durationInMinutes - days * MINUTES_PER_DAY) / MINUTES_PER_HOUR);
+    const minutes = durationInMinutes - days * MINUTES_PER_DAY - hours * MINUTES_PER_HOUR;
+    return `${toFormattedString(days, 2)}D ${toFormattedString(hours, 2)}H ${toFormattedString(minutes, 2)}M`;
+  }
+};
+
+const createTripPointTemplate = (point) => {
+  const {type, destination, price, startTime, endTime, offers, isFavorite} = point;
+
+  const date = dayjs(startTime).format(`MMM D`);
+  const eventStartTime = dayjs(startTime).format(`HH:mm`);
+  const eventEndTime = dayjs(endTime).format(`HH:mm`);
+
+  const createOfferTemplate = (offer) => {
+    return `
+    <li class="event__offer">
+      <span class="event__offer-title">${offer.title}</span>
+      &plus;&nbsp;&euro;&nbsp;<span class="event__offer-price">${offer.price}</span>
+    </li>`;
+  };
+
+  const favoriteBtnClass = isFavorite ?
+    `event__favorite-btn event__favorite-btn--active` :
+    `event__favorite-btn`;
+
+  const createOffersTemplate = () => offers.map((element) => createOfferTemplate(element)).join(``);
+
   return `
     <li class="trip-events__item">
       <div class="event">
-        <time class="event__date" datetime="2019-03-19">MAR 19</time>
+        <time class="event__date" datetime="${startTime.toISOString()}">${date}</time>
         <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/flight.png" alt="Event type icon">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
         </div>
-        <h3 class="event__title">Flight Geneva</h3>
+        <h3 class="event__title">${type} ${destination}</h3>
         <div class="event__schedule">
           <p class="event__time">
-            <time class="event__start-time" datetime="2019-03-19T18:00">18:00</time>
+            <time class="event__start-time" datetime="${startTime.toISOString()}">${eventStartTime}</time>
             &mdash;
-            <time class="event__end-time" datetime="2019-03-19T19:00">19:00</time>
+            <time class="event__end-time" datetime="${endTime.toISOString()}">${eventEndTime}</time>
           </p>
-          <p class="event__duration">1H</p>
+          <p class="event__duration">${getEventDuration(startTime, endTime)}</p>
         </div>
         <p class="event__price">
-          &euro;&nbsp;<span class="event__price-value">20</span>
+          &euro;&nbsp;<span class="event__price-value">${price}</span>
         </p>
         <h4 class="visually-hidden">Offers:</h4>
         <ul class="event__selected-offers">
-          <li class="event__offer">
-            <span class="event__offer-title">Add luggage</span>
-            &plus;&euro;&nbsp;
-            <span class="event__offer-price">30</span>
-          </li>
-          <li class="event__offer">
-            <span class="event__offer-title">Switch to comfort</span>
-            &plus;&euro;&nbsp;
-            <span class="event__offer-price">100</span>
-          </li>
+          ${createOffersTemplate()}
         </ul>
         <button class="event__favorite-btn" type="button">
           <span class="visually-hidden">Add to favorite</span>
@@ -37,7 +70,7 @@ const createTripPointTemplate = () => {
             <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
           </svg>
         </button>
-        <button class="event__rollup-btn" type="button">
+        <button class="${favoriteBtnClass}" type="button">
           <span class="visually-hidden">Open event</span>
         </button>
       </div>
