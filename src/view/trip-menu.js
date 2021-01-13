5c221890a846d09fe9b58cbe3d6b055f1807ed60
mAnic <arctic.manic@gmail.com>
2021-01-13 20:58:26 +0300

    module4-task1

--- src/view/trip-menu.js
+++ src/view/trip-menu.js
@@ -1,4 +1,4 @@
-import {createElement} from "../utils.js";
+import AbstractView from "./abstract.js";
 
 const createTripMenuTemplate = () => {
   return `
@@ -9,24 +9,8 @@ const createTripMenuTemplate = () => {
   `;
 };
 
-export default class TripMenu {
-  constructor() {
-    this._element = null;
-  }
-
+export default class TripMenu extends AbstractView {
   getTemplate() {
     return createTripMenuTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
